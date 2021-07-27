680d8b7e665ff009e2790cdb8df914b9d87a523c
rasulalizadeh <mohammadr.alizadeh@live.com>
2021-07-27 16:03:12 +0430

    5Mordad

--- bank_turn/bank.py
+++ bank_turn/bank.py
@@ -3,6 +3,12 @@ from datetime import datetime
 # ##############functions #############
 
 
+def press(label_name):
+    global counter_list
+    if counter_list:
+        con[label_name]["textvariable"].set(counter_list.pop(0))
+
+
 def get_time():
     dt = datetime.now()
     return dt.strftime("%H:%M:%S")
@@ -100,9 +106,9 @@ operators = tk.Toplevel()
 operators.geometry("500x200")
 operators.title("operators")
 
-tk.Button(operators, cnf=con["op1"]).grid(row=0, column=0, padx=35, pady=10)
-tk.Button(operators, cnf=con["op2"]).grid(row=0, column=1, padx=35, pady=10)
-tk.Button(operators, cnf=con["op3"]).grid(row=0, column=2, padx=35, pady=10)
+tk.Button(operators, cnf=con["op1"], command=lambda: press("lop1")).grid(row=0, column=0, padx=35, pady=10)
+tk.Button(operators, cnf=con["op2"], command=lambda: press("lop2")).grid(row=0, column=1, padx=35, pady=10)
+tk.Button(operators, cnf=con["op3"], command=lambda: press("lop3")).grid(row=0, column=2, padx=35, pady=10)
 
 tk.Label(operators, cnf=con["lop1"]).grid(row=1, column=0)
 tk.Label(operators, cnf=con["lop2"]).grid(row=1, column=1)
