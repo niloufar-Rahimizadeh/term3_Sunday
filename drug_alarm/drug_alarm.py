df20482f820c5a15aac324478d40e50de2cb54ba
Student Space <niloufarrahimizade@yahoo.com>
2021-08-01 16:01:26 +0430

    10Mordad

--- drug_alarm/drug_alarm.py
+++ drug_alarm/drug_alarm.py
@@ -2,6 +2,17 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 root = tk.Tk()
+root.geometry('200x200')
 
+note = ttk.Notebook(root)
+note.grid(row=0, column=0)
+patients = ttk.Frame(note)
+note.add(patients, text="patients")
+timer = ttk.Frame(note)
+note.add(timer, text="timer")
 
+lf1 = tk.LabelFrame(patients, text="Patient 1")
+lf1.grid(row=0, column=0)
+tk.Label(lf1, text="Name").grid(row=0, column=0)
+tk.Label(lf1, text="Time").grid(row=1, column=0)
 root.mainloop()
