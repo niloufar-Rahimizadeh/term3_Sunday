98a92d3b76a01d1754e4339a6f4dbe93e22ff76e
rasulalizadeh <mohammadr.alizadeh@live.com>
2021-08-03 16:03:57 +0430

    12Mordad

--- drug_alarm/drug_alarm.py
+++ drug_alarm/drug_alarm.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 root = tk.Tk()
-root.geometry('200x200')
+root.geometry('240x240')
 
 note = ttk.Notebook(root)
 note.grid(row=0, column=0)
@@ -10,9 +10,25 @@ patients = ttk.Frame(note)
 note.add(patients, text="patients")
 timer = ttk.Frame(note)
 note.add(timer, text="timer")
-
+# patient 1
 lf1 = tk.LabelFrame(patients, text="Patient 1")
 lf1.grid(row=0, column=0)
 tk.Label(lf1, text="Name").grid(row=0, column=0)
+tk.Entry(lf1, width=10).grid(row=0, column=1)
 tk.Label(lf1, text="Time").grid(row=1, column=0)
+
+# patient 2
+lf2 = tk.LabelFrame(patients, text="Patient 2")
+lf2.grid(row=1, column=0)
+tk.Label(lf2, text="Name").grid(row=0, column=0)
+tk.Entry(lf2, width=10).grid(row=0, column=1)
+tk.Label(lf2, text="Time").grid(row=1, column=0)
+# patient 3
+lf3 = tk.LabelFrame(patients, text="Patient 3")
+lf3.grid(row=2, column=0)
+tk.Label(lf3, text="Name").grid(row=0, column=0)
+tk.Entry(lf3, width=10).grid(row=0, column=1)
+tk.Label(lf3, text="Time").grid(row=1, column=0)
+#################################
+
 root.mainloop()
